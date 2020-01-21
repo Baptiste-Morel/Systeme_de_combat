d87db8c897fe40a4f5a46a52087102b28aa92585
Baptiste-Morel <baptiste.morel82170@gmail.com>
2020-01-21 21:51:49 +0100

    Exercice 1

--- truc.c
+++ truc.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
 int main(){
-	
-	printf("Bonjour !\n");
-	
+	int hpJoueur;
+	int attaqueSimple;
+
+	hpJoueur=100;
+	attaqueSimple=50;
+
+	printf("Un Monstre mechant pas beau apparait.\n");
+	printf("Vous avez 100 points de vie \n");
+	printf("Le monstre vous lance une attaque simple, vous perdez 50 points de vie.\n");
+	hpJoueur -= attaqueSimple;
+	printf("Il vous reste ");
+	printf("%d",hpJoueur);
+	printf(" points de vie");
+
 	return 0;
 }
